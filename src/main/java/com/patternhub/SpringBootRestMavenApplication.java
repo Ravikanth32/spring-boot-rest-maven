c5f71a59f3a351bdbbf6fdeaf57ce31e9a2e1d84
ravik <rkondepati32@gmail.com>
2023-06-25 12:20:22 +0530

    added home controller

---
+++ src/main/java/com/patternhub/SpringBootRestMavenApplication.java
@@ -0,0 +1,13 @@
+package com.patternhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestMavenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/patternhub/SpringBootRestMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patternhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
